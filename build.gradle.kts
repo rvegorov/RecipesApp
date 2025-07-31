f810017ff9059798c1ea75c36899a4ef8aef03ed
REgorov <eogorovromanv@hotmail.com>
2025-07-31 16:25:26 +0300

    RA-29 Enable Safe Args alternative

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.9.3" apply false
 }
 
-buildscript {
-    repositories {
-        google()
-    }
-    dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.9.3")
-    }
-}
