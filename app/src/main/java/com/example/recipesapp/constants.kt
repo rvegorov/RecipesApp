a996c10012e6a2dce5ead55cd6f69bdb80d57776
REgorov <eogorovromanv@hotmail.com>
2025-08-04 13:36:36 +0300

    RA-32 implement a request via URLConnection, decode category list

--- app/src/main/java/com/example/recipesapp/constants.kt
+++ app/src/main/java/com/example/recipesapp/constants.kt
@@ -1,6 +1,5 @@
 package com.example.recipesapp
 
-const val ARG_CATEGORY_ID = "ARG_CATEGORY_ID"
-const val ARG_RECIPE_ID = "ARG_RECIPE_ID"
 const val SP_NAME = "favourites"
 const val FAVOURITES_IDS_KEY = "favouritesIdsSet"
+const val API_URL = "https://recipes.androidsprint.ru/api/"
